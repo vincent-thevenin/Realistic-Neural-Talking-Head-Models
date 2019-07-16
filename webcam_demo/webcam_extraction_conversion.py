fff975f2c949bf0924694fc6667c6cbbad0b147d
vincent-thevenin <thevenin.vincent@hotmail.com>
2019-07-16 17:20:33 +0200

    Added webcam extraction functions and inference

---
+++ webcam_demo/webcam_extraction_conversion.py
@@ -0,0 +1,126 @@
+import cv2
+import face_alignment
+from matplotlib import pyplot as plt
+import numpy as np
+import torch
+
+def get_borders(preds):
+    minX = maxX = preds[0,0]
+    minY = maxY = preds[0,1]
+    
+    for i in range(1, len(preds)):
+        x = preds[i,0]
+        if x < minX:
+            minX = x
+        elif x > maxX:
+            maxX = x
+        
+        y = preds[i,1]
+        if y < minY:
+            minY = y
+        elif y > maxY:
+            maxY = y
+    
+    return minX, maxX, minY, maxY
+
+def crop_and_reshape_preds(preds, pad, out_shape=256):
+    minX, maxX, minY, maxY = get_borders(preds)
+    
+    maxDelta = max(maxX - minX, maxY - minY)
+    
+    #find reshape factor
+    r = out_shape/(maxDelta+2*pad)
+        
+    for i in range(len(preds)):
+        preds[i,0] = r*(preds[i,0] - minX + (maxDelta-(maxX - minX))/2 + pad)
+        preds[i,1] = r*(preds[i,1] - minY + (maxDelta-(maxY - minY))/2 + pad)
+    return preds
+
+def crop_and_reshape_img(img, preds, pad, out_shape=256):
+    minX, maxX, minY, maxY = get_borders(preds)
+    
+    #find reshape factor
+    delta = max(maxX - minX, maxY - minY)
+    deltaX = (delta - (maxX - minX))/2
+    deltaY = (delta - (maxY - minY))/2
+    
+    minX = int(minX)
+    maxX = int(maxX)
+    minY = int(minY)
+    maxY = int(maxY)
+    deltaX = int(deltaX)
+    deltaY = int(deltaY)
+    img = img[minY-deltaY-pad:maxY+deltaY+pad, minX-deltaX-pad:maxX+deltaX+pad, :]
+    img = cv2.resize(img, (out_shape,out_shape))
+    
+    return img
+
+"""Input: cap a cv2.VideoCapture object, device the torch.device, 
+pad the distance in pixel from border to face
+
+output: x the camera output, g_y the corresponding landmark"""
+def generate_landmarks(cap, device, pad):
+    #Get webcam image
+    
+    fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False, device='cuda:0')
+    no_pic = True
+    
+    while(no_pic == True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frames_list = [RGB]
+
+
+        #Create landmark for face
+        frame_landmark_list = []
+
+        for i in range(len(frames_list)):
+            try:
+                input = frames_list[i]
+                preds = fa.get_landmarks(input)[0]
+                
+                input = crop_and_reshape_img(input, preds, pad=pad)
+                preds = crop_and_reshape_preds(preds, pad=pad)
+                
+                dpi = 100
+                fig = plt.figure(figsize=(256/dpi, 256/dpi), dpi = dpi)
+                ax = fig.add_subplot(1,1,1)
+                ax.imshow(np.ones(input.shape))
+                plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+
+                #chin
+                ax.plot(preds[0:17,0],preds[0:17,1],marker='',markersize=5,linestyle='-',color='green',lw=2)
+                #left and right eyebrow
+                ax.plot(preds[17:22,0],preds[17:22,1],marker='',markersize=5,linestyle='-',color='orange',lw=2)
+                ax.plot(preds[22:27,0],preds[22:27,1],marker='',markersize=5,linestyle='-',color='orange',lw=2)
+                #nose
+                ax.plot(preds[27:31,0],preds[27:31,1],marker='',markersize=5,linestyle='-',color='blue',lw=2)
+                ax.plot(preds[31:36,0],preds[31:36,1],marker='',markersize=5,linestyle='-',color='blue',lw=2)
+                #left and right eye
+                ax.plot(preds[36:42,0],preds[36:42,1],marker='',markersize=5,linestyle='-',color='red',lw=2)
+                ax.plot(preds[42:48,0],preds[42:48,1],marker='',markersize=5,linestyle='-',color='red',lw=2)
+                #outer and inner lip
+                ax.plot(preds[48:60,0],preds[48:60,1],marker='',markersize=5,linestyle='-',color='purple',lw=2)
+                ax.plot(preds[60:68,0],preds[60:68,1],marker='',markersize=5,linestyle='-',color='pink',lw=2) 
+                ax.axis('off')
+
+                fig.canvas.draw()
+
+                data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+                data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+                frame_landmark_list.append((input, data))
+                plt.close(fig)
+                no_pic = False
+            except:
+                print('Error: Video corrupted or no landmarks visible')
+
+    
+    frame_mark = torch.from_numpy(np.array(frame_landmark_list)).type(dtype = torch.float) #K,2,224,224,3
+    frame_mark = frame_mark.transpose(2,4).to(device) #K,2,3,224,224
+    
+    x = frame_mark[0,0].to(device)
+    g_y = frame_mark[0,1].to(device)
+    
+    return x,g_y
---
+++ webcam_inference.py
@@ -0,0 +1,83 @@
+import torch
+import cv2
+from matplotlib import pyplot as plt
+
+from loss.loss_discriminator import *
+from loss.loss_generator import *
+from network.blocks import *
+from network.model import *
+from webcam_demo.webcam_extraction_conversion import *
+
+"""Init"""
+
+#Paths
+path_to_model_weights = 'model_weights.tar'
+path_to_embedding = 'e_hat.tar'
+
+device = torch.device("cuda:0")
+cpu = torch.device("cpu")
+
+checkpoint = torch.load(path_to_model_weights, map_location=cpu) 
+e_hat = torch.load(path_to_embedding, map_location=cpu)
+e_hat = e_hat['e_hat'].to(device)
+
+G = Generator(256, finetuning=True, e_finetuning=e_hat)
+G.eval()
+
+"""Training Init"""
+G.load_state_dict(checkpoint['G_state_dict'])
+G.to(device)
+G.finetuning_init()
+
+
+"""Main"""
+print('PRESS Q TO EXIT')
+cap = cv2.VideoCapture(0)
+
+with torch.no_grad():
+    while True:
+        x, g_y = generate_landmarks(cap=cap, device=device, pad=50)
+
+        g_y = g_y.unsqueeze(0)
+        x = x.unsqueeze(0)
+
+
+        #forward
+        # Calculate average encoding vector for video
+        #f_lm_compact = f_lm.view(-1, f_lm.shape[-4], f_lm.shape[-3], f_lm.shape[-2], f_lm.shape[-1]) #BxK,2,3,224,224
+        #train G
+
+        x_hat = G(g_y, e_hat)
+
+        plt.clf()
+        out1 = x_hat.transpose(1,3)[0]/255
+        #for img_no in range(1,x_hat.shape[0]):
+        #    out1 = torch.cat((out1, x_hat.transpose(1,3)[img_no]), dim = 1)
+        out1 = out1.to(cpu).numpy()
+        #plt.imshow(out1)
+        #plt.show()
+        
+        #plt.clf()
+        out2 = x.transpose(1,3)[0]/255
+        #for img_no in range(1,x.shape[0]):
+        #    out2 = torch.cat((out2, x.transpose(1,3)[img_no]), dim = 1)
+        out2 = out2.to(cpu).numpy()
+        #plt.imshow(out2)
+        #plt.show()
+
+        #plt.clf()
+        out3 = g_y.transpose(1,3)[0]/255
+        #for img_no in range(1,g_y.shape[0]):
+        #    out3 = torch.cat((out3, g_y.transpose(1,3)[img_no]), dim = 1)
+        out3 = out3.to(cpu).numpy()
+        #plt.imshow(out3)
+        #plt.show()
+        
+        cv2.imshow('fake', cv2.cvtColor(out1, cv2.COLOR_BGR2RGB))
+        cv2.imshow('me', cv2.cvtColor(out2, cv2.COLOR_BGR2RGB))
+        cv2.imshow('ladnmark', cv2.cvtColor(out3, cv2.COLOR_BGR2RGB))
+        
+        if cv2.waitKey(1) == ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows()
