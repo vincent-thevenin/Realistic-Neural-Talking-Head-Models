Fixed loss of gradient

dataset/video_extraction_conversion.py
@@ -137,7 +137,7 @@ def generate_cropped_landmarks(frames_list, pad=50):
             ax.axis('off')
 
             fig.canvas.draw()
-
+    
             data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
             data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
 

loss/loss_generator.py
@@ -27,9 +27,9 @@ class LossCnt(nn.Module):
 
         #define hook
         def vgg_x_hook(module, input, output):
-            vgg_x_features.append(output.data)
+            vgg_x_features.append(output)
         def vgg_xhat_hook(module, input, output):
-            vgg_xhat_features.append(output.data)
+            vgg_xhat_features.append(output)
 
         """same as above for vggface"""
         vgg_x_features = []