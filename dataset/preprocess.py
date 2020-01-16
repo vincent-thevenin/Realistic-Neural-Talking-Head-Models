80cd41d40591e33b4b0094bc450de57d5b73fabc
Vincent <thevenin.vincent@hotmail.com>
2020-01-16 16:15:37 +0100

    Save 1 large image instead of multiple

--- dataset/preprocess.py
+++ dataset/preprocess.py
@@ -1,14 +1,16 @@
-from video_extraction_conversion import *
 import torch
 import os
 from datetime import datetime
 import numpy as np
 import cv2
-from torchvision.utils import save_image
+# from torchvision.utils import save_image
 from tqdm import tqdm
+import face_alignment
+from matplotlib import pyplot as plt
 
 path_to_mp4 = '/mnt/ACA21355A21322FE/VoxCeleb/vox2_mp4/dev/mp4'
 K = 8
+num_vid = 0
 device = torch.device('cuda:0')
 saves_dir = '/mnt/ACA21355A21322FE/VoxCeleb/saves'
 face_aligner = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False, device ='cuda:0')
@@ -16,7 +18,55 @@ face_aligner = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, fl
 if not os.path.isdir(saves_dir):
     os.mkdir(saves_dir)
 
-def save_images(video_path, pic_folder, num_images):
+def generate_landmarks(frames_list, face_aligner):
+    frame_landmark_list = []
+    fa = face_aligner
+    
+    for i in range(len(frames_list)):
+        try:
+            input = frames_list[i]
+            preds = fa.get_landmarks(input)[0]
+
+            dpi = 100
+            fig = plt.figure(figsize=(input.shape[1]/dpi, input.shape[0]/dpi), dpi = dpi)
+            ax = fig.add_subplot(1,1,1)
+            ax.imshow(np.ones(input.shape))
+            plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+
+            #chin
+            ax.plot(preds[0:17,0],preds[0:17,1],marker='',markersize=5,linestyle='-',color='green',lw=2)
+            #left and right eyebrow
+            ax.plot(preds[17:22,0],preds[17:22,1],marker='',markersize=5,linestyle='-',color='orange',lw=2)
+            ax.plot(preds[22:27,0],preds[22:27,1],marker='',markersize=5,linestyle='-',color='orange',lw=2)
+            #nose
+            ax.plot(preds[27:31,0],preds[27:31,1],marker='',markersize=5,linestyle='-',color='blue',lw=2)
+            ax.plot(preds[31:36,0],preds[31:36,1],marker='',markersize=5,linestyle='-',color='blue',lw=2)
+            #left and right eye
+            ax.plot(preds[36:42,0],preds[36:42,1],marker='',markersize=5,linestyle='-',color='red',lw=2)
+            ax.plot(preds[42:48,0],preds[42:48,1],marker='',markersize=5,linestyle='-',color='red',lw=2)
+            #outer and inner lip
+            ax.plot(preds[48:60,0],preds[48:60,1],marker='',markersize=5,linestyle='-',color='purple',lw=2)
+            ax.plot(preds[60:68,0],preds[60:68,1],marker='',markersize=5,linestyle='-',color='pink',lw=2) 
+            ax.axis('off')
+
+            fig.canvas.draw()
+
+            data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+            data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+            frame_landmark_list.append((input, data))
+            plt.close(fig)
+        except:
+            print('Error: Video corrupted or no landmarks visible')
+    
+    for i in range(len(frames_list) - len(frame_landmark_list)):
+        #filling frame_landmark_list in case of error
+        frame_landmark_list.append(frame_landmark_list[i])
+    
+    
+    return frame_landmark_list
+
+def pick_images(video_path, pic_folder, num_images):
     cap = cv2.VideoCapture(video_path)
     
     n_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -36,9 +86,9 @@ def save_images(video_path, pic_folder, num_images):
             RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             frames_list.append(RGB)
             
-            frame_counter += 1
-            pic_path = pic_folder + '_'+str(frame_counter)+'.jpg'
-            cv2.imwrite(pic_path, frame)
+            # frame_counter += 1
+            # pic_path = pic_folder + '_'+str(frame_counter)+'.jpg'
+            # cv2.imwrite(pic_path, frame)
             
         frame_idx += 1
 
@@ -46,59 +96,35 @@ def save_images(video_path, pic_folder, num_images):
     
     return frames_list
     
-    
-progress = 0
-total = 145568
-mean = torch.Tensor([0.485, 0.456, 0.406]).unsqueeze(0).expand(K*2,3).reshape(-1).to(device)
-std = torch.Tensor([0.229, 0.224, 0.225]).unsqueeze(0).expand(K*2,3).reshape(-1).to(device)
-elapsed_time = datetime.now()   
-elapsed_time -= elapsed_time
-timer_start = datetime.now()
+
 for person_id in tqdm(os.listdir(path_to_mp4)):
     for video_id in os.listdir(os.path.join(path_to_mp4, person_id)):
         for video in os.listdir(os.path.join(path_to_mp4, person_id, video_id)):
             
-            progress += 1
-            
-            if not os.path.isdir(saves_dir+'/' + person_id):
-                os.mkdir(saves_dir+'/' + person_id)
-            if not os.path.isdir(saves_dir +'/'+ person_id+'/'+video_id):
-                os.mkdir(saves_dir +'/'+ person_id+'/'+video_id)
-            elif len(os.listdir(saves_dir +'/'+ person_id+'/'+video_id)) == 16:
-                total -= 1
-                progress -= 1
-                break
                 
             try:
                 video_path = os.path.join(path_to_mp4, person_id, video_id, video)
-                frame_mark = save_images(video_path, saves_dir+'/' + person_id+'/'+video_id+'/'+video.split('.')[0], K)
+                frame_mark = pick_images(video_path, saves_dir+'/' + person_id+'/'+video_id+'/'+video.split('.')[0], K)
                 frame_mark = generate_landmarks(frame_mark, face_aligner)
-                for i, frame in enumerate(frame_mark):
-                    save_image(torch.from_numpy(frame[1]).type(dtype = torch.float).transpose(0,2).transpose(1,2), saves_dir+'/' + person_id+'/'+video_id+'/LM'+'_'+str(i)+'.jpg')
-                # frame_mark = torch.from_numpy(np.array(frame_mark)).type(dtype = torch.float) #K,2,224,224,3
-    #             frame_mark = frame_mark.transpose(2,4).to(device) #K,2,3,224,224
-    #             frame_mark = frame_mark.view(-1,frame_mark.shape[-1],frame_mark.shape[-1])/255
-    
-    # #            frame_mark = normalize(frame_mark, mean, std, inplace=True)
-    #             frame_mark = frame_mark.view(K, 2, 3, frame_mark.shape[-1], frame_mark.shape[-1])
-                
-    #             g_idx = torch.randint(low = 0, high = K, size = (1,1))
-    #             x = frame_mark[g_idx,0].squeeze()
-    #             g_y = frame_mark[g_idx,1].squeeze()
-    #             torch.save({
-    #                 'frame_mark': frame_mark,
-    #                 'x': x,
-    #                 'g_y':g_y,
-    #                 }, saves_dir +'/'+ person_id+'/'+video_id+'/'+video.split('.')[0]+'.pth')
-                # if progress%(total//10000+1)==1:
-                #     timer_stop = datetime.now()
-                #     diff_time = timer_stop - timer_start
-                #     timer_start = timer_stop
-                #     elapsed_time += diff_time
-                #     print('ETA:',elapsed_time*(total/progress - 1),'\n')
+                if len(frame_mark) == K:
+                    final_list = [frame_mark[i][0] for i in range(K)]
+                    for i in range(K):
+                        final_list.append(frame_mark[i][1]) #K*2,224,224,3
+                    final_list = np.array(final_list)
+                    final_list = np.transpose(final_list, [1,0,2,3])
+                    final_list = np.reshape(final_list, (224, 224*2*K, 3))
+                    final_list = cv2.cvtColor(final_list, cv2.COLOR_BGR2RGB)
+                    
+                    if not os.path.isdir(saves_dir+"/"+str(num_vid//256)):
+                        os.mkdir(saves_dir+"/"+str(num_vid//256))
+                        
+                    cv2.imwrite(saves_dir+"/"+str(num_vid//256)+"/"+str(num_vid)+".jpg", final_list)
+                    num_vid += 1
+                    break #take only one video
+
+                    
             except:
                 print('ERROR: ', video_path)
             
-            break
         
 print('done')
