09d14c5cc30e0189bb223044d3157e8f2d2b381b
Vincent <thevenin.vincent@hotmail.com>
2020-01-29 12:48:13 +0100

    Use relu instead of max

--- loss/loss_discriminator.py
+++ loss/loss_discriminator.py
@@ -7,18 +7,22 @@ class LossDSCreal(nn.Module):
     """
     def __init__(self):
         super(LossDSCreal, self).__init__()
+        self.relu = nn.ReLU()
         
     def forward(self, r):
-        loss = torch.max(torch.zeros_like(r), 1 - r)
+        # loss = torch.max(torch.zeros_like(r), 1 - r)
+        loss = self.relu(1.0-r)
         return loss.mean()
 
 class LossDSCfake(nn.Module):
     """
-    Inputs: r, rhat
+    Inputs: rhat
     """
     def __init__(self):
         super(LossDSCfake, self).__init__()
+        self.relu = nn.ReLU()
         
     def forward(self, rhat):
-        loss = torch.max(torch.zeros_like(rhat),1 + rhat)
+        # loss = torch.max(torch.zeros_like(rhat),1 + rhat)
+        loss = self.relu(1.0+rhat)
         return loss.mean()
