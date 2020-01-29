f3f9308c5ea4301796d3d9890bafcf9cc7473b30
Vincent <thevenin.vincent@hotmail.com>
2020-01-29 12:46:47 +0100

    Normalize finetuning dataset loader

--- dataset/dataset_class.py
+++ dataset/dataset_class.py
@@ -115,8 +115,8 @@ class FineTuningImagesDataset(Dataset):
         frame_mark_images = torch.from_numpy(np.array(frame_mark_images)).type(dtype = torch.float) #1,2,256,256,3
         frame_mark_images = frame_mark_images.transpose(2,4).to(self.device) #1,2,3,256,256
         
-        x = frame_mark_images[0,0].squeeze()
-        g_y = frame_mark_images[0,1].squeeze()
+        x = frame_mark_images[0,0].squeeze()/255
+        g_y = frame_mark_images[0,1].squeeze()/255
         
         return x, g_y
         
