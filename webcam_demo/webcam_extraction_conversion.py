1d25febb64934fdf7037273e3a43b24da619eba8
vincent-thevenin <thevenin.vincent@hotmail.com>
2020-04-06 15:08:30 +0000

    Add bool for frame read at return tuple for generate landmark

--- webcam_demo/webcam_extraction_conversion.py
+++ webcam_demo/webcam_extraction_conversion.py
@@ -83,59 +83,63 @@ output: x the camera output, g_y the corresponding landmark"""
     while(no_pic == True):
         # Capture frame-by-frame
         ret, frame = cap.read()
-        RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frames_list = [RGB]
+        if ret:
+            RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frames_list = [RGB]
 
 
-        #Create landmark for face
-        frame_landmark_list = []
+            #Create landmark for face
+            frame_landmark_list = []
 
-        for i in range(len(frames_list)):
-            try:
-                input = frames_list[i]
-                preds = fa.get_landmarks(input)[0]
-                
-                input = crop_and_reshape_img(input, preds, pad=pad)
-                preds = crop_and_reshape_preds(preds, pad=pad)
-                
-                dpi = 100
-                fig = plt.figure(figsize=(256/dpi, 256/dpi), dpi = dpi)
-                ax = fig.add_subplot(1,1,1)
-                ax.imshow(np.ones(input.shape))
-                plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+            for i in range(len(frames_list)):
+                try:
+                    input = frames_list[i]
+                    preds = fa.get_landmarks(input)[0]
 
-                #chin
-                ax.plot(preds[0:17,0],preds[0:17,1],marker='',markersize=5,linestyle='-',color='green',lw=2)
-                #left and right eyebrow
-                ax.plot(preds[17:22,0],preds[17:22,1],marker='',markersize=5,linestyle='-',color='orange',lw=2)
-                ax.plot(preds[22:27,0],preds[22:27,1],marker='',markersize=5,linestyle='-',color='orange',lw=2)
-                #nose
-                ax.plot(preds[27:31,0],preds[27:31,1],marker='',markersize=5,linestyle='-',color='blue',lw=2)
-                ax.plot(preds[31:36,0],preds[31:36,1],marker='',markersize=5,linestyle='-',color='blue',lw=2)
-                #left and right eye
-                ax.plot(preds[36:42,0],preds[36:42,1],marker='',markersize=5,linestyle='-',color='red',lw=2)
-                ax.plot(preds[42:48,0],preds[42:48,1],marker='',markersize=5,linestyle='-',color='red',lw=2)
-                #outer and inner lip
-                ax.plot(preds[48:60,0],preds[48:60,1],marker='',markersize=5,linestyle='-',color='purple',lw=2)
-                ax.plot(preds[60:68,0],preds[60:68,1],marker='',markersize=5,linestyle='-',color='pink',lw=2) 
-                ax.axis('off')
+                    input = crop_and_reshape_img(input, preds, pad=pad)
+                    preds = crop_and_reshape_preds(preds, pad=pad)
 
-                fig.canvas.draw()
+                    dpi = 100
+                    fig = plt.figure(figsize=(256/dpi, 256/dpi), dpi = dpi)
+                    ax = fig.add_subplot(1,1,1)
+                    ax.imshow(np.ones(input.shape))
+                    plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
 
-                data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
-                data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+                    #chin
+                    ax.plot(preds[0:17,0],preds[0:17,1],marker='',markersize=5,linestyle='-',color='green',lw=2)
+                    #left and right eyebrow
+                    ax.plot(preds[17:22,0],preds[17:22,1],marker='',markersize=5,linestyle='-',color='orange',lw=2)
+                    ax.plot(preds[22:27,0],preds[22:27,1],marker='',markersize=5,linestyle='-',color='orange',lw=2)
+                    #nose
+                    ax.plot(preds[27:31,0],preds[27:31,1],marker='',markersize=5,linestyle='-',color='blue',lw=2)
+                    ax.plot(preds[31:36,0],preds[31:36,1],marker='',markersize=5,linestyle='-',color='blue',lw=2)
+                    #left and right eye
+                    ax.plot(preds[36:42,0],preds[36:42,1],marker='',markersize=5,linestyle='-',color='red',lw=2)
+                    ax.plot(preds[42:48,0],preds[42:48,1],marker='',markersize=5,linestyle='-',color='red',lw=2)
+                    #outer and inner lip
+                    ax.plot(preds[48:60,0],preds[48:60,1],marker='',markersize=5,linestyle='-',color='purple',lw=2)
+                    ax.plot(preds[60:68,0],preds[60:68,1],marker='',markersize=5,linestyle='-',color='pink',lw=2) 
+                    ax.axis('off')
 
-                frame_landmark_list.append((input, data))
-                plt.close(fig)
-                no_pic = False
-            except:
-                print('Error: Video corrupted or no landmarks visible')
+                    fig.canvas.draw()
 
+                    data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+                    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+                    frame_landmark_list.append((input, data))
+                    plt.close(fig)
+                    no_pic = False
+                except:
+                    print('Error: Video corrupted or no landmarks visible')
+        else:
+            break
+    if ret:
+        frame_mark = torch.from_numpy(np.array(frame_landmark_list)).type(dtype = torch.float) #K,2,256,256,3
+        frame_mark = frame_mark.transpose(2,4).to(device) #K,2,3,256,256
+
+        x = frame_mark[0,0].to(device)
+        g_y = frame_mark[0,1].to(device)
+    else:
+        x = g_y = None
     
-    frame_mark = torch.from_numpy(np.array(frame_landmark_list)).type(dtype = torch.float) #K,2,256,256,3
-    frame_mark = frame_mark.transpose(2,4).to(device) #K,2,3,256,256
-    
-    x = frame_mark[0,0].to(device)
-    g_y = frame_mark[0,1].to(device)
-    
-    return x,g_y
+    return x,g_y,ret
