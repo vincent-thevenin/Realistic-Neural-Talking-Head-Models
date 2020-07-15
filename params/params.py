e43ca9fccc297ca048544722aab1a3ee4e9b59f5
Vincent Thévenin <thevenin.vincent@hotmail.com>
2020-07-15 17:13:50 +0200

    Added path to mp4

--- params/params.py
+++ params/params.py
@@ -1,4 +1,4 @@
-#K, path_to_chkpt, path_to_backup, path_to_Wi, batch_size, path_to_preprocess, frame_shape
+#K, path_to_chkpt, path_to_backup, path_to_Wi, batch_size, path_to_preprocess, frame_shape, path_to_mp4
 
 #number of frames to load
 K = 8
@@ -21,3 +21,6 @@ path_to_preprocess = '/mnt/ACA21355A21322FE/VoxCeleb/saves2'
 
 #default for Voxceleb
 frame_shape = 224
+
+#path to dataset
+path_to_mp4 = '/mnt/ACA21355A21322FE/VoxCeleb/vox2_mp4/dev/mp4'
